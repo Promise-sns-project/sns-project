36aae9d762a09440c921d34d940808633fad2f53
HubCreator <chocomilk8604@gmail.com>
2022-05-05 10:11:04 +0900

    해시태그 페이지에서, 자신이 추가했던 해시태그 목록 보기

--- routes/favor.js
+++ routes/favor.js
@@ -16,16 +16,42 @@ router.post("/", isLoggedIn, async (req, res, next) => {
         if (favors) {
             const result = await Promise.all(
                 favors.map((tag) => {
+                    // favor 생성
                     return Favor.findOrCreate({
                         where: { favor: tag.slice(1).toLowerCase() },
                     });
                 })
             );
 
-            await user.addFavor(result.map((r) => r[0]));
+            await user.addFavor(result.map((r) => r[0])); // favor의 연관 관계 설정
         }
 
         res.redirect("/");
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+router.get("/", isLoggedIn, async (req, res, next) => {
+    try {
+        // TODO : 현재 로그인된 사용자의 favor 기반으로 posts들을 필터링한 결과를 보여줌
+        const user = await User.findOne({
+            where: { id: req.user.id },
+            include: [
+                {
+                    model: Favor,
+                },
+            ],
+        }); // 현재 로그인된 사용자 정보
+
+        const favors = user.Favors;
+
+        return res.render("hashtagPage", {
+            title: `hashtagPage`,
+            user,
+            favors,
+        });
     } catch (error) {
         console.log(error);
         next(error);
