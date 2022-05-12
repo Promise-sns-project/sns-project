f73d97605b33db978a8937a8953115139429b7d1
Doyeon04 <rlaehdus0411@naver.com>
2022-05-12 13:09:50 +0900

    피드 게시글 삭제

--- routes/post.js
+++ routes/post.js
@@ -9,55 +9,65 @@ const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
 
 try {
-    fs.readdirSync("uploads");
+  fs.readdirSync("uploads");
 } catch (error) {
-    console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
-    fs.mkdirSync("uploads");
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
 }
 
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, cb) {
-            cb(null, "uploads/");
-        },
-        filename(req, file, cb) {
-            const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024 },
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
-    console.log(req.file);
-    res.json({ url: `/img/${req.file.filename}` });
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` });
 });
 
 const upload2 = multer();
 router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
-    try {
-        console.log(req.user);
-        const post = await Post.create({
-            content: req.body.content,
-            img: req.body.url,
-            UserId: req.user.id,
-        });
-        const hashtags = req.body.content.match(/#[^\s#]*/g);
-        if (hashtags) {
-            const result = await Promise.all(
-                hashtags.map((tag) => {
-                    return Hashtag.findOrCreate({
-                        where: { title: tag.slice(1).toLowerCase() },
-                    });
-                })
-            );
-            await post.addHashtags(result.map((r) => r[0]));
-        }
-        res.redirect("/");
-    } catch (error) {
-        console.error(error);
-        next(error);
+  try {
+    console.log(req.user);
+    const post = await Post.create({
+      content: req.body.content,
+      img: req.body.url,
+      UserId: req.user.id,
+    });
+    const hashtags = req.body.content.match(/#[^\s#]*/g);
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map((tag) => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          });
+        })
+      );
+      await post.addHashtags(result.map((r) => r[0]));
     }
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Post.destroy({ where: { id: req.params.id, userId: req.user.id } }); // userId - 내가 쓴 게시글인지 확인
+    res.status(200).json();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
